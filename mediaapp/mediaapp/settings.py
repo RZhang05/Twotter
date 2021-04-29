b644b998a1919b0258b41bf4b2a79b4c320c1c63
Jonathan Sumabat <diaxaxx@gmail.com>
2021-04-29 14:59:37 -0400

    Update settings for custom User model

--- mediaapp/mediaapp/settings.py
+++ mediaapp/mediaapp/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'mediaapp.apps.TwotterConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -83,6 +84,8 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
+
+AUTH_USER_MODEL = 'mediaapp.User'
 
 AUTH_PASSWORD_VALIDATORS = [
     {

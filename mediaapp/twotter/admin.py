9755f7a0129b1aed017478feef925a02cebfa26d
Raymond <zhangraymond05@gmail.com>
2021-04-29 15:59:05 -0400

    Admin page with user

--- mediaapp/twotter/admin.py
+++ mediaapp/twotter/admin.py
@@ -1,11 +1,5 @@
 from django.contrib import admin
-from django.contrib.auth import get_user_model
 
-User = get_user_model()
+from .models import User
 
-
-class UserAdmin:
-    fields = ('username', 'first_name', 'last_name', 'password', 'date_of_birth', 'date_joined', 'status', 'img_name')
-
-
-admin.register(User, UserAdmin)
+admin.site.register(User)

0c1fd13e04e4413c7fefa2f47088e0469b71ee82
Raymond <zhangraymond05@gmail.com>
2021-04-29 15:19:42 -0400

    Updated models.py

--- mediaapp/twotter/models.py
+++ mediaapp/twotter/models.py
@@ -3,4 +3,6 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    date_of_birth = models.DateField
+    date_of_birth = models.DateField(null=True, blank=True)
+    status = models.TextField(max_length=100,blank=True)
+    img_name = models.TextField(max_length=500,blank=True)

7fdf58cd7f6807d70b175c27f8e1354b1396ccb0
Jonathan Sumabat <diaxaxx@gmail.com>
2021-04-30 17:04:15 -0400

    Remove unneeded fields

--- mediaapp/twotter/admin.py
+++ mediaapp/twotter/admin.py
@@ -1,12 +1,10 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import User as CustomUser
 
 
 class CustomUserAdmin(UserAdmin):
-    form = CustomUserChangeForm
     fieldsets = (
         ('Account Info', {'fields': ('email', 'username', 'password', 'date_joined')}),
         ('Personal Info', {'fields': ('first_name', 'last_name', 'date_of_birth')}),
--- mediaapp/twotter/forms.py
+++ mediaapp/twotter/forms.py
@@ -3,14 +3,3 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import User
 
 
-class CustomUserCreationForm(UserCreationForm):
-    class Meta:
-        model = User
-        fields = ('date_of_birth', 'status', 'img_name',)
-
-
-class CustomUserChangeForm(UserChangeForm):
-    class Meta:
-        model = User
-        fields = ('date_of_birth', 'status', 'img_name',)
-

66924af70da62c6c9fd3684cf9806b139fa8ed67
Raymond <zhangraymond05@gmail.com>
2021-05-04 15:02:50 -0400

    Integrated channels for later use.

--- mediaapp/mediaapp/settings.py
+++ mediaapp/mediaapp/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+	'channels',
 ]
 
 MIDDLEWARE = [
@@ -70,7 +71,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'mediaapp.wsgi.application'
-
+ASGI_APPLICATION = 'mediaapp.asgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
